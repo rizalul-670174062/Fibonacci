e8b75ae9d2a2d522a6154cea227a559273aaf5d8
mohammad rizalul fikri <48679687+rizalul-670174062@users.noreply.github.com>
2019-04-02 08:54:20 +0700

    Update Jurnal8.java

--- jurnal8/src/jurnal8/Jurnal8.java
+++ jurnal8/src/jurnal8/Jurnal8.java
@@ -12,6 +12,24 @@ import java.util.Scanner;
  * @author Praktikan
  */
 public class Jurnal8 {
+      static int calculateSum(int n) 
+    { 
+        if (n <= 0) 
+           return 0; 
+        int fibo[]=new int[n+1]; 
+        fibo[0] = 0; fibo[1] = 1; 
+       
+        // Initialize result 
+        int sum = fibo[0] + fibo[1]; 
+       
+        // Add remaining terms 
+        for (int i=2; i<=n; i++) 
+        { 
+            fibo[i] = fibo[i-1]+fibo[i-2]; 
+            sum += fibo[i]; 
+        }
+        return sum; 
+    } 
 
     /**
      * @param args the command line arguments
